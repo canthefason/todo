a9516767b21f3fc060605082c57cb34253009889
Can Yucel <can.yucel@gmail.com>
2016-03-01 01:37:02 -0800

    scripts: update manifest file paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,13 @@ set -e
 kubeargs="--namespace=$KUBE_ENV"
 
 shopt -s expand_aliases
-alias kubectl="sudo docker run --rm -v /home/core/.kube:/root/.kube wernight/kubectl kubectl"
+alias kubectl="sudo docker run --rm -v /home/core/.kube:/root/.kube -v /home/core/pipelines/todo-service:/root/todo-service wernight/kubectl kubectl"
 
 set +e
 rcExist=$(kubectl get -o template rc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs)
 set -e
 if [ "$rcExist" != "ReplicationController" ]; then
-  kubectl create -f scripts/rc.yml $kubeargs
+  kubectl create -f /root/todo-service/scripts/rc.yml $kubeargs
 else
   kubectl rolling-update $GO_PIPELINE_NAME --image=canthefason/$GO_PIPELINE_NAME:$GO_PIPELINE_LABEL --update-period=20s $kubeargs
 fi
@@ -18,5 +18,5 @@ set +e
 svcExist=$(kubectl get -o template svc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs)
 set -e
 if [ "$svcExist" != "Service" ]; then
-  kubectl create -f scripts/svc.yml $kubeargs
+  kubectl create -f /root/todo-service/scripts/svc.yml $kubeargs
 fi
