d099d8207c77ec32523a0d1d937013b79375d4d1
Can Yucel <can.yucel@gmail.com>
2016-03-02 00:08:38 -0800

    fix environment variable substitution in manifest files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,18 +5,16 @@ kubeargs="--namespace=$KUBE_ENV"
 shopt -s expand_aliases
 alias kubectl="sudo docker run --rm -v /home/core/.kube:/root/.kube -v /home/core/pipelines/todo-service:/root/todo-service wernight/kubectl kubectl"
 
-set +e
-rcExist=$(kubectl get -o template rc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs)
-set -e
+rcExist=$(kubectl get -o template rc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs) || true
 if [ "$rcExist" != "ReplicationController" ]; then
-  kubectl create -f /root/todo-service/scripts/rc.yml $kubeargs
+  envsubst < scripts/rc.yml > kubetemp.yml
+  cat kubetemp.yaml
+  kubectl create -f /root/todo-service/kubetemp.yml $kubeargs
 else
   kubectl rolling-update $GO_PIPELINE_NAME --image=canthefason/$GO_PIPELINE_NAME:$GO_PIPELINE_LABEL --update-period=20s $kubeargs
 fi
 
-set +e
-svcExist=$(kubectl get -o template svc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs)
-set -e
+svcExist=$(kubectl get -o template svc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs) || true
 if [ "$svcExist" != "Service" ]; then
   kubectl create -f /root/todo-service/scripts/svc.yml $kubeargs
 fi
